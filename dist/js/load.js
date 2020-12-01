dcd934ada6461b03c63a9621353e0e467f5df54d
kaki <74591174+kakigawa-1219@users.noreply.github.com>
2020-12-01 10:26:05 +0900

    Update load.js

--- dist/js/load.js
+++ dist/js/load.js
@@ -1,15 +1,51 @@
 $(function() {
-    $.getJSON("include.json" , function(data) {
-      var
-        ulObj = $("#ul-user"),
-        len = data.length;
-  
-      for(var i = 0; i < len; i++) {
-        ulObj.append($("<li>").attr({"id":data[i].number}).text(data[i].lastName));
-      }
-    });
+   
+  $.ajax({
+    type: 'GET',
+    url: 'https://kakigawa-1219.github.io/js-Exercise7/dist/js/include.json',
+    dataType: 'json',
+    success: function(json){
+      var len = json.length;
+     
+             
+      for(var i=0; i < len; i++){
+        var htmltext =  '<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center">'
+        +   '<div class="thumbnail">'
+        +       '<div class="thumbnail-image">'
+        +           '<img class="img-circle" src="dist/img/jinbutsu_male.jpg">'
+        +       '</div>'
+        +       '<div class="user-indicator"></div>'
+        +       `<div class="user-name">${json[i].lastName}${json[i].firstName}</div>`
+        +       `<div class="user-division">${json[i].division}</div>`
+        +       `<div class="user-position">${json[i].position}</div>`
+        +      '<div class="text-center"></div>'
+        +   '</div>'
+        + '</li>';
+
+        var htmltext1 =  '<li class="user-box col-md-2 col-sm-4 col-xs-12 text-center">'
+        +   '<div class="thumbnail">'
+        +       '<div class="thumbnail-image">'
+        +           '<img class="img-circle" src="dist/img/jinbutsu_female.jpg">'
+        +       '</div>'
+        +       '<div class="user-indicator"></div>'
+        +       `<div class="user-name">${json[i].lastName}${json[i].firstName}</div>`
+        +       `<div class="user-division">${json[i].division}</div>`
+        +       `<div class="user-position">${json[i].position}</div>`
+        +      '<div class="text-center"></div>'
+        +   '</div>'
+        + '</li>';
+
+        if(json[i].gender=="男"){
+            $("#loadarea").append(htmltext);
+        }
+        else{
+            $("#loadarea").append(htmltext1);
+        }
+    
+    }
+       
+    }
+    
+  });
   });
 
-
-
-  
